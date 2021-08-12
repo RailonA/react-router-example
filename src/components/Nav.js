57f0e0b5dca2c56593e2a0ef94d58cb778af2e6b
RailonA <railonacosta@gmail.com>
2021-08-12 18:31:52 -0400

    Add style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  };
+
   return (
     <nav>
       <h3>Logo</h3>
       <ul className="nav-link">
-        <Link to="/">
+        <Link style={navStyle} to="/">
           <li>Home</li>
         </Link>
-        <Link to="/app">
+        <Link style={navStyle} to="/app">
           <li>App</li>
         </Link>
-        <Link to="/profile">
+        <Link style={navStyle} to="/profile">
           <li>Profile</li>
         </Link>
       </ul>
